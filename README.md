61c8fef164fb2d0324dc0b0ffa1e685d9842c28e
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-08-25 09:17:06 +0900

    トップのタイトルのCSSを変更

--- README.md
+++ README.md
@@ -111,3 +111,5 @@ Visit [Nx Cloud](https://nx.app/) to learn more.
 13. nx run api:serve <!-- バックエンド側のサーバー立てる>
 14. nx run quiz:serve<!-- フロントエンドエンド側のサーバー立てる>
 　　
+<!-- TODO https://github.com/Kahoot-Clone/kahoot-clone見てどうやって異なるURLのウインドウで1つのページでのウインドウでのイベント発火で別のウインドウの変更を加えることができるか把握 -->
+<!-- TODO <Link/>を使って「問題です」の出題ページに -->
--- apps/quiz/pages/index.tsx
+++ apps/quiz/pages/index.tsx
@@ -13,10 +13,15 @@ const db = firebase.firestore();
 
 const TopBackGroundImg = styled.section``;
 
+const TopTitle = styled.h1`
+  font-family: 'Dela Gothic One', cursive;
+  font-size: 8rem;
+  font-weight: 400;
+`;
+
 const TopTitlePart = styled.span`
   display: inline-block;
   position: relative;
-  /* color: transparent; */
   background-clip: border-box;
   background: linear-gradient(
     ${colors.titleOrange},
@@ -24,23 +29,19 @@ const TopTitlePart = styled.span`
     ${colors.titleOrange}
   );
   -webkit-background-clip: text; //テキストでくり抜く
-  -webkit-text-fill-color: transparent;//くり抜いた部分は背景を表示
+  -webkit-text-fill-color: transparent; //くり抜いた部分は背景を表示
   &::after {
     background: none;
     content: attr(data-text);
     left: 0;
     position: absolute;
-    text-shadow: 6px 6px 1px ${colors.titlePurple}, -6px -6px 1px ${colors.titlePurple};
+    text-shadow: 6px 6px 1px ${colors.titlePurple},
+      -6px -6px 1px ${colors.titlePurple};
     top: 0;
     z-index: -1;
   }
 `;
 
-const TopTitle = styled.h1`
-  font-family: 'Dela Gothic One', cursive;
-  font-size: 8rem;
-  font-weight: 400;
-`;
 // db.collection()
 
 const Index = () => {
