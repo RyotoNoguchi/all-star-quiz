064c518faa21e94d0fd2aca600d3d86d724341ba
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-10-03 10:22:37 +0900

    AddQuestionのバリデーション改修

--- apps/quiz/components/organisms/AddQuestion/index.tsx
+++ apps/quiz/components/organisms/AddQuestion/index.tsx
@@ -83,15 +83,21 @@ const validate = (values: FormValueType): FormErrorType => {
   return errors
 };
 
+yup.addMethod<yup.NumberSchema>(yup.number, "noWhitespace", function () {
+  return this.transform((value, originalValue) => (/\s/.test(originalValue) ? NaN : value));
+});
+
 const validationSchema = yup.object({
-  questionId: yup.string().required('問題番号を入力してください。'),
-  question: yup.string().required('問題文を入力してください。'),
+  questionId: yup.number().required('問題番号を入力してください。').integer('問題番号は整数を入力してください').min(1, '問題番号は1以上を入力してください').max(100, '登録できる問題数は100個までです。')?.noWhitespace(),
+  // ↑ https://github.com/jquense/yup/issues/312#issuecomment-745034006（TSでのメソッドの定義追加）
+  // ↑ https://github.com/jquense/yup/issues/694#issuecomment-663613804（noWhitespace()を追加するコード）
+  question: yup.string().strict().trim('空欄はあかんで。').required('問題文を入力してください。'),
   answer: yup.string().required('問題の正解を入力してください。'),
   choices: yup.object({
-    A: yup.string().required('Aに入力してください'),
-    B: yup.string().required('Bに入力してください'),
-    C: yup.string().required('Cに入力してください'),
-    D: yup.string().required('Dに入力してください'),
+    A: yup.string().strict().trim('空欄はあかんで。').required('Aに入力してください'),
+    B: yup.string().strict().trim('空欄はあかんで。').required('Bに入力してください'),
+    C: yup.string().strict().trim('空欄はあかんで。').required('Cに入力してください'),
+    D: yup.string().strict().trim('空欄はあかんで。').required('Dに入力してください'),
   })
 })
 
@@ -115,8 +121,6 @@ const AddQuestion: React.FC = () => {
     onSubmit,
     validationSchema
   });
-
-  console.log(formik.touched);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     setChoice(e.target.value as Choice)
