7d85a0714f2636bb319eb9732c91669246695796
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-11-21 20:27:38 +0900

    ローカルホストをherokuに立てたwebサーバーに変更

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,9 +16,9 @@ async function bootstrap() {
   // app.useStaticAssets('../../quiz/pages/index.tsx');
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = "https://all-star-quiz-api.herokuapp.com/" || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log('Listening at https://all-star-quiz-api.herokuapp.com/' + port + '/' + globalPrefix);
   });
 }
 
