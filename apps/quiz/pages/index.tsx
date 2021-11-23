a627fb118375a896c8737df7554c1469aa70e4ca
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-11-24 08:09:10 +0900

    '/'で'/monitor/question/1'にリダイレクトするように修正

--- apps/quiz/pages/index.tsx
+++ apps/quiz/pages/index.tsx
@@ -1,8 +1,13 @@
 import Box from '@mui/material/Box';
-import React from 'react';
+import React, { useEffect } from 'react';
+import router from 'next/router';
 import { TopTitle, TopTitlePart } from '../components/molecules/TopTitle';
 
+
 const Index: React.VFC = () => {
+  useEffect(() => {
+    router.push('/monitor/question/1')
+  }, [])
   const title = 'アソビュー オールスター感謝祭 2021';
   const titleArray = title.split(' ');
   const title1stRow = titleArray[0];
