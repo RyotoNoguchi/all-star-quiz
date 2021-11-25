54e9adf8226fe2780d42fa495aa9353783f5df61
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-11-26 08:39:14 +0900

    バグ解消

--- apps/quiz/pages/index.tsx
+++ apps/quiz/pages/index.tsx
@@ -1,13 +1,13 @@
 import Box from '@mui/material/Box';
-import React, { useEffect } from 'react';
+import React from 'react';
 import router from 'next/router';
 import { TopTitle, TopTitlePart } from '../components/molecules/TopTitle';
 
 
 const Index: React.VFC = () => {
-  useEffect(() => {
-    router.push('/monitor/question/1')
-  }, [])
+  // useEffect(() => {
+  //   router.push('/monitor/question/1')
+  // }, [])
   const title = 'アソビュー オールスター感謝祭 2021';
   const titleArray = title.split(' ');
   const title1stRow = titleArray[0];
