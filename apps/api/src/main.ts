3b8d0db6ae9a6ccda3c1f49563d751bd30d8f52d
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2021-11-30 11:06:22 +0900

    main.tsを編集

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,11 +11,15 @@ const API_BASE_URL = "https://all-star-quiz-api.herokuapp.com/"
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.useStaticAssets('../../quiz/pages/index.tsx');
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = "https://all-star-quiz-api.herokuapp.com/" || 3333;
   await app.listen(port, () => {
-    Logger.log(`Listening at ${API_BASE_URL} + '/' + globalPrefix`);
+    Logger.log(`Listening at https://all-star-quiz-api.herokuapp.com/ + ${port} + '/' + globalPrefix`);
   });
 }
 
